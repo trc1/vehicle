f61affe0341f0187a2c490c3008ae57d3737024d
fakofbolan <dario.trcovic@gmail.com>
2024-04-09 07:43:03 +0200

    apiServices

--- src/Services/VehicleMakeServices.js
+++ src/Services/VehicleMakeServices.js
@@ -1,9 +1,29 @@
-import ApiServices from './ApiServices';
+import ApiService from './ApiService';
 
-class VehicleMakeServices extends ApiServices {
+class VehicleMakeService extends ApiService {
   constructor() {
     super('https://api.baasic.com/v1/test234/resources/vehicleMake');
     //env
+  }
+
+  async fetchVehicleMakes(queryParams = {}) {    
+    try {
+      const response = await fetch(params);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data from fetchVehicleMakes');
+      }
+
+      const data = await response.json();
+
+      var mappedModel = {
+        vehicleId: data.id
+      }
+
+      return mappedModel;
+    } catch (error) {
+      console.error('Error fetching data from fetchVehicleMakes:', error);
+      throw error;
+    }
   }
 }
 
